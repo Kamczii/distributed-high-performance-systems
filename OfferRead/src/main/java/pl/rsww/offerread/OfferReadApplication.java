a3703e2d737d393aca269c78d866d4b26af13680
k4mczi <kamil.czepiell@gmail.com>
2024-05-02 14:05:52 +0200

    simple offer read init

--- OfferRead/src/main/java/pl/rsww/offerread/OfferReadApplication.java
+++ OfferRead/src/main/java/pl/rsww/offerread/OfferReadApplication.java
@@ -4,10 +4,16 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import pl.rsww.offerread.locations.getting_locations.Location;
 import pl.rsww.offerread.locations.getting_locations.LocationRepository;
 import pl.rsww.offerread.offers.getting_offers.OfferShortInfoProjection;
 import pl.rsww.offerread.offers.getting_offers.OfferShortInfoRepository;
 import pl.rsww.offerread.offers.getting_offers.ShortInfoService;
+import pl.rsww.offerwrite.api.AvailableOrderStatus;
+import pl.rsww.offerwrite.api.OfferIntegrationEvent;
+
+import java.time.LocalDate;
+import java.util.UUID;
 
 @SpringBootApplication
 public class OfferReadApplication {
@@ -19,25 +25,24 @@ public class OfferReadApplication {
     public CommandLineRunner run(OfferShortInfoProjection projection, OfferShortInfoRepository shortInfoRepository, ShortInfoService shortInfoService,
                                  LocationRepository locationRepository) {
         return args -> {
-//            OfferIntegrationEvent.Created offerEvent = new OfferIntegrationEvent.Created(
-//                    UUID.fromString("b01a4efb-8255-490b-bee6-b0e00c2768df"),  // offerId
-//                    new OfferIntegrationEvent.Hotel(
-//                            "Ritz Carlton",  // hotel name
-//                            new OfferIntegrationEvent.Room("Suite", 2, 2)  // room type, capacity, beds
-//                    ),
-//                    new OfferIntegrationEvent.Location("New York", "USA"),  // departure location
-//                    new OfferIntegrationEvent.Location("Paris", "France"),  // destinationCity location
-//                    LocalDate.of(2024, 4, 15),  // start date
-//                    LocalDate.of(2024, 4, 25),   // end date,
-//                    AvailableOrderStatus.OPEN
-//            );
-//            projection.listenOffer(offerEvent);
-//
-//            shortInfoRepository.findAll().forEach(System.out::println);
-//
-//            shortInfoService.search(new GetOffers());
             shortInfoRepository.deleteAll();
             locationRepository.deleteAll();
+            locationRepository.insert(new Location("New York", "USA"));
+            locationRepository.insert(new Location("Paris", "France"));
+
+            OfferIntegrationEvent.Created offerEvent = new OfferIntegrationEvent.Created(
+                    UUID.fromString("b01a4efb-8255-490b-bee6-b0e00c2768df"),  // offerId
+                    new OfferIntegrationEvent.Hotel(
+                            "Ritz Carlton",  // hotel name
+                            new OfferIntegrationEvent.Room("Suite", 2, 2)  // room type, capacity, beds
+                    ),
+                    new OfferIntegrationEvent.Location("New York", "USA"),  // departure location
+                    new OfferIntegrationEvent.Location("Paris", "France"),  // destinationCity location
+                    LocalDate.of(2024, 4, 15),  // start date
+                    LocalDate.of(2024, 4, 25),   // end date,
+                    AvailableOrderStatus.OPEN
+            );
+            projection.listenOffer(offerEvent);
         };
     }
 }
