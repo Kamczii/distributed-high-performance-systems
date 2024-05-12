06005e020e75c58aeeddf23539546f8f618b4049
dimatkchnk <dimiktk@gmail.com>
2024-05-12 14:56:59 +0200

    Fix and reworked communication OfferWrite <-> Order <-> Payment

--- Order/src/main/java/pl/rsww/order/core/config/KafkaConsumerConfig.java
+++ Order/src/main/java/pl/rsww/order/core/config/KafkaConsumerConfig.java
@@ -2,12 +2,14 @@ package pl.rsww.order.core.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 import pl.rsww.offerwrite.api.response.OfferResponse;
 import pl.rsww.order.core.deserializer.GenericObjectDeserializer;
 import pl.rsww.payment.api.PaymentEvent;
@@ -17,15 +19,19 @@ import java.util.Map;
 
 @Configuration
 public class KafkaConsumerConfig {
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapAddress;
 
     @Bean
     public <T> ConsumerFactory<String, T> consumerFactory(Class<T> targetType) {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "order-group");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, GenericObjectDeserializer.class);
-        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), new GenericObjectDeserializer<>(targetType));
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        JsonDeserializer<T> deserializer = new JsonDeserializer<>(targetType);
+        deserializer.addTrustedPackages("pl.rsww.offerwrite.api.response");
+        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), deserializer);
     }
 
     @Bean
--- Payment/src/main/java/pl/rsww/payment/core/config/KafkaConsumerConfig.java
+++ Payment/src/main/java/pl/rsww/payment/core/config/KafkaConsumerConfig.java
@@ -2,11 +2,13 @@ package pl.rsww.payment.core.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 import pl.rsww.offerwrite.api.response.OfferResponse;
 import pl.rsww.order.api.OrderEvent;
 import pl.rsww.payment.core.deserializer.GenericObjectDeserializer;
@@ -17,15 +19,19 @@ import java.util.Map;
 
 @Configuration
 public class KafkaConsumerConfig {
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapAddress;
 
     @Bean
     public <T> ConsumerFactory<String, T> consumerFactory(Class<T> targetType) {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "payment-group");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, GenericObjectDeserializer.class);
-        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), new GenericObjectDeserializer<>(targetType));
+        JsonDeserializer<T> deserializer = new JsonDeserializer<>(targetType);
+        deserializer.addTrustedPackages("pl.rsww.order.api");
+        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), deserializer);
     }
 
     @Bean
