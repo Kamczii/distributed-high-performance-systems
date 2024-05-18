68fe6252cbcf92d675cea247ee559271447a7ef3
k4mczi <kamil.czepiell@gmail.com>
2024-05-18 16:39:56 +0200

    script for local docker development

---
+++ run.bat
@@ -0,0 +1,23 @@
+@echo off
+REM Stop and remove all running containers
+docker-compose down
+
+REM Define the services to build and run
+set SERVICES=frontend offer_write offer_read kafka event_store postgres notification eureka gateway
+
+REM Remove all Docker volumes except projekt_postgres_data
+for /F "delims=" %%i in ('docker volume ls -q') do (
+    if "%%i" neq "projekt_postgres_data" docker volume rm %%i
+)
+
+REM Remove all Docker images
+docker rmi -f $(docker images -aq)
+
+REM Build
+docker-compose build %SERVICES%
+
+REM Run the containers
+docker-compose up -d %SERVICES%
+
+echo Script execution completed.
+pause
