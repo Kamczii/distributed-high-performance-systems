de09529596432bf2a925c4f7cffe7ec6780c173e
k4mczi <kamil.czepiell@gmail.com>
2024-06-08 20:20:47 +0200

    useful scripts

--- rerun.bat
+++ rerun.bat
@@ -3,18 +3,12 @@ REM Stop and remove all running containers
 docker-compose down
 
 REM Define the services to build and run
-set SERVICES=frontend notification eureka gateway nginx offer_read offer_write payment order tour_operator
+set SERVICES=notification eureka gateway offer_read offer_write payment order
 
 REM Remove all Docker volumes except projekt_postgres_data
 for /F "delims=" %%i in ('docker volume ls -q') do (
     if "%%i" neq "projekt_postgres_data" docker volume rm %%i
 )
-
-REM Remove all Docker images
-docker rmi -f $(docker images -aq)
-
-REM Build
-docker-compose build %SERVICES%
 
 REM Run the containers
 docker-compose up -d %SERVICES%
