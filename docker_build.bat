217676b0ce7ce4e0e23677b50b7deea955267b14
k4mczi <kamil.czepiell@gmail.com>
2024-06-11 07:18:41 +0200

    commented clr

--- docker_build.bat
+++ docker_build.bat
@@ -1,8 +1,8 @@
-docker build ./Frontend  --no-cache -t k444mczi/rsww_180066_frontend:2.0
+docker build ./Frontend  --no-cache -t k444mczi/rsww_180066_frontend:2.1
 docker build ./Gateway  --no-cache -t k444mczi/rsww_180066_gateway:2.0
 docker build .  --no-cache -f ./Notifications/Dockerfile -t k444mczi/rsww_180066_notification:2.0
 docker build .  --no-cache -f ./OfferRead/Dockerfile -t k444mczi/rsww_180066_offerread:2.0
 docker build .  --no-cache -f ./OfferWrite/Dockerfile -t k444mczi/rsww_180066_offerwrite:2.0
 docker build .  --no-cache -f ./Order/Dockerfile -t k444mczi/rsww_180066_order:2.0
 docker build .  --no-cache -f ./Payment/Dockerfile -t k444mczi/rsww_180066_payment:2.0
-docker build .  --no-cache -f ./TourOperator/Dockerfile -t k444mczi/rsww_180066_touroperator:2.0
+docker build . -f ./TourOperator/Dockerfile -t k444mczi/rsww_180066_touroperator:2.0
--- docker_push.bat
+++ docker_push.bat
@@ -1,8 +1,8 @@
-docker push k444mczi/rsww_180066_frontend:2.0
 docker push k444mczi/rsww_180066_gateway:2.0
 docker push k444mczi/rsww_180066_notification:2.0
 docker push k444mczi/rsww_180066_offerread:2.0
 docker push k444mczi/rsww_180066_offerwrite:2.0
 docker push k444mczi/rsww_180066_order:2.0
 docker push k444mczi/rsww_180066_payment:2.0
-docker push k444mczi/rsww_180066_touroperator
+docker push k444mczi/rsww_180066_frontend:2.1
+docker push k444mczi/rsww_180066_touroperator:2.0
